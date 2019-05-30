08c09dbfabf44770ae63d939933544248a2d3ab2
Pandassi <47264697+pandassi@users.noreply.github.com>
2019-05-30 14:05:51 -0400

    Update TestFenetre.java

--- src/TestFenetre.java
+++ src/TestFenetre.java
@@ -6,9 +6,6 @@ import pkgmonframe.MonFrame;
  
 public class TestFenetre {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
          //Fenetre  f=new Fenetre();
 //         MonFrame mf=new MonFrame();
